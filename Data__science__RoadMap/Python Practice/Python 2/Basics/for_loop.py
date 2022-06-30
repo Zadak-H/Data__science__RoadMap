edb057eab2938c690fac55fdeec7910608ae14f8
--replace-all <rajarshi921@gmail.com>
2022-06-30 14:45:55 +0530

    Simple programs

---
+++ Data__science__RoadMap/Python Practice/Python 2/Basics/for_loop.py
@@ -0,0 +1,6 @@
+friends = ['Raj','Ajay','Suman','Avik','jay']
+for friend in friends:
+    print(friend)
+
+for index in range(1,10):
+    print(index)
---
+++ Data__science__RoadMap/Python Practice/Python 2/Basics/return.py
@@ -0,0 +1,5 @@
+def cube(num):
+    return pow(num,3)
+n = 3
+X = cube(n)
+print(str(n) +" ^ 3 = "+str(X))
